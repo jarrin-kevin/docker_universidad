d1dcd7dabf034dafd3a6bc2e75173c228db8b519
jarrin-kevin <63314112+jarrin-kevin@users.noreply.github.com>
2025-05-05 03:29:09 -0500

    Delete Downloads/docker_universidad-main/docker_universidad-main directory

--- Downloads/docker_universidad-main/docker_universidad-main/mysql-init/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS universidad;
--- Use the universidad database
-USE universidad;
-
--- Grant privileges
-GRANT ALL PRIVILEGES ON universidad.* TO 'root'@'%';
-FLUSH PRIVILEGES;
